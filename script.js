9418a675b0b2934a5f9328ffb0ce851690f5c62e
Victor Lee <myleevictor@gmail.com>
2018-03-24 11:17:53 +0800

    passed function test using jest

---
+++ script.js
@@ -0,0 +1,23 @@
+function find_most_frequent(arr) {
+  let map = {};
+
+  if (arr.length === 0) return 0;
+
+  arr.forEach(element => {
+    if (map[element] === undefined) {
+      map[element] = 1;
+    } else {
+      map[element]++;
+    }
+  });
+
+  const result = Object.keys(map).reduce((elem1, elem2) => {
+    return map[elem1] > map[elem2] ? elem1 : elem2;
+  });
+
+  return result;
+}
+
+find_most_frequent([1, 1, "2", "2", "2", 3, 4, -1, -1, -1, -1, -1, -1]);
+
+module.exports = find_most_frequent
